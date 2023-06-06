be8c12d286e7593eed9aacdd4d98c17ea3da3dc2
DavKiin <davipontes38@hotmail.com>
2023-06-06 01:29:18 -0300

    Cypress addition + First Test
    Cypress Addition:
    - Install dependece and cypress.
    - Realize initial configuration.
    - Start the tests.
    - Deployed script test containing "note-list"  and ''create-list'' page.
    
    Tests:
    - Identify elements on list-note page.
    - click "add note" button.
    - Check URLs
    - Change page
    - Realize insert, delete, verification in Text Areas.
    - Localize Save and Cancel Button
    - Redirecting after click to Save or Cancel button.

---
+++ training_notes/backend/Cypress/cypress/e2e/noteTest.cy.js
@@ -0,0 +1,86 @@
+const isEmpty  = " ";
+
+describe('Note List and add test pag. Puchase', () => {
+        //Test to identify initial phrase to the screen
+    it('Access main "notelist" and localize all graphic inserts on the screen', () => {
+        cy.visit('http://localhost:4200/list-note'); //Visiting Note Page
+        cy.contains('There are no thoughts registered yet!').should('be.visible'); // Verify a text contain
+        cy.get('button.buttom').should('be.visible');
+    })
+        //Addition note Test.
+    it('Access note page and adding note located by class:', () => {
+        cy.visit('http://localhost:4200/list-note'); //Visiting Note Page
+        cy.get('button.buttom').click(); //Click the button.
+        cy.url().should('eq','http://localhost:4200/create-note') // checks if the URL that has been directed is the same as declared.  
+    })
+        //Addition note Test 
+    it('Access note page and adding note located by Button Name ', () => {
+        cy.visit('http://localhost:4200/list-note'); //Visiting Note Page
+        cy.contains('Add notes').should('be.visible').click(); //Click the button.
+    })
+  })
+
+
+describe('Creating notes Test' , () => {
+    it('Checks the Text Label', () => {
+        cy.visit('http://localhost:4200/create-note'); //Visiting Note Page
+        cy.url().should('eq','http://localhost:4200/create-note'); //URL Check.
+        cy.get('input#note').should('be.visible'); //Find "Note" text Area
+        cy.get('input#origin').should('be.visible'); //Find "Origin" text Area
+    })
+
+    it('Check Text in TExtArea', () => {
+        cy.visit('http://localhost:4200/create-note'); //Visiting Note Page
+        cy.url().should('eq','http://localhost:4200/create-note'); //URL Check.
+
+            //If TextArea have something write, it will return an error;
+        cy.get('input#note').should('eq','');         
+        cy.get('input#origin').should('eq','');
+    })
+    
+    it('Insert Text', () => {
+        cy.visit('http://localhost:4200/create-note'); //Visiting Note Page
+        cy.url().should('eq','http://localhost:4200/create-note'); //URL Check.
+            //Input String on text area
+        cy.get('input#note').type('Anotação simples que devera estar dentro da parte de anotações');
+        cy.get('input#origin').type('Aqui fica a origem da anotação');
+    })
+
+    it('Delete text', () => {
+        cy.visit('http://localhost:4200/create-note'); //Visiting Note Page
+        cy.url().should('eq','http://localhost:4200/create-note'); //URL Check.
+            //Delete anyone string in Text Area
+        cy.get('input#note').type('{selectall}{backspace}');
+        cy.get('input#origin').type('{selectall}{backspace}');
+            // Checks if area is clear
+            cy.get('input#note').should('have.value','');         
+            cy.get('input#origin').should('have.value','');
+    })
+
+    it('Choose Category', () => {
+        
+    })
+
+    it('Save Note', () => {
+        cy.visit('http://localhost:4200/create-note'); //Visiting Note Page
+        cy.url().should('eq','http://localhost:4200/create-note'); //URL Check.
+            
+        cy.contains('Save').should('be.visible');
+        cy.contains('Save').click(); // Click in save
+            //After clicking in save, the app needs to return to list note page
+        cy.url().should('eq','http://localhost:4200/list-note'); //Check URL after click
+    })
+
+    it('Cancel Note', () => {
+        cy.visit('http://localhost:4200/create-note'); //Visiting Note Page
+        cy.url().should('eq','http://localhost:4200/create-note'); //URL Check.
+        cy.contains('Cancel').should('be.visible');
+        cy.contains('Cancel').click(); // Click in save
+            //After clicking in save, the app needs to return to list note page
+        cy.url().should('eq','http://localhost:4200/list-note'); //Check URL after click    
+    
+    })
+}
+)
+
+  
